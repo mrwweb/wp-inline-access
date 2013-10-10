9e8d4b2794ad7807837db46d91edfb8e2a4a35f0
mrwweb <info@mrwweb.com>
2013-10-10 14:27:00 -0700

    shift js functions out of ready and load. improve target styles in admin

--- js/wpia.js
+++ js/wpia.js
@@ -5,6 +5,64 @@ $.fn.exists = function () {
     return this.length !== 0;
 }
 
+function wpiaToggleEditMode() {
+	$editableItems = $('.wpia-is-editable');
+	if( $('body').hasClass('wpia-toggled') ) {
+		$editableItems.each( function() {
+			$(this)
+			.wrap('<a class="wpia-edit-link" href="' + $(this).data('wpia-edit-href') + '">')
+			.tooltip({
+				items: '[data-wpia-edit-tooltip]',
+				content: function() {
+					return $(this).data('wpia-edit-tooltip');
+				},
+				tooltipClass: 'wpia-tooltip',
+				track: true
+			});
+		});
+	} else {
+		$editableItems.each( function() {
+			$(this).tooltip('destroy').unwrap();
+		});
+	}
+}
+
+// function for expanding targeted widgets in the admin
+// differs from generic target script to handle
+function expandTargetedWidget() {
+	target = window.location.hash;
+
+	if( ! target )
+		return;
+
+	//strip "#"
+	target = target.replace(/^.*#wpia-/, '');
+
+	$targetedWidget = $('.widget[id$=' + target + ']');
+
+	// Stop if nothing's targeted
+	if( !$targetedWidget.exists() )
+		return;
+
+	$targetedSidebar = $targetedWidget.parents('.widgets-holder-wrap');
+
+	// Expand sidebar of targeted widget if closed
+	if( $targetedSidebar.hasClass('closed') ) {
+		// expand sidebar
+		$('.sidebar-name-arrow', $targetedSidebar).trigger('click');
+	}
+
+	// scroll to the widget
+    $('html, body').animate({
+        scrollTop: $targetedWidget.offset().top - 50 // -50 accounts for admin bar
+    }, 750);
+
+    // expand widget
+	$('.widget-action', $targetedWidget).trigger('click');
+	// add class to mark as targeted
+	$targetedWidget.addClass('wpia-active-widget');
+}
+
 function wpiaTargetToggle() {
 	// get hash
 	var hash = window.location.hash;
@@ -37,30 +95,7 @@ $(document).ready(function (){
 	
 	var $body = $('body'),
 		$wpiaToggleButton = $('#wp-admin-bar-wpia-toggle-edit-mode a'),
-		$infobar = $('.wpia-info-bar'),
-		$editableItems = $('.wpia-is-editable');
-
-	function wpiaToggleEditMode() {
-		if( $body.hasClass('wpia-toggled') ) {
-			$editableItems.each( function() {
-				$(this)
-				.wrap('<a class="wpia-edit-link" href="' + $(this).data('wpia-edit-href') + '">')
-				.tooltip({
-					items: '[data-wpia-edit-tooltip]',
-					content: function() {
-						return $(this).data('wpia-edit-tooltip');
-					},
-					tooltipClass: 'wpia-tooltip',
-					track: true
-				});
-				console.log($(this).data('wpia-edit-tooltip'));
-			});
-		} else {
-			$editableItems.each( function() {
-				$(this).tooltip('destroy').unwrap();
-			});
-		}
-	}
+		$infobar = $('.wpia-info-bar');
 
 	// Trigger things when the Edit Mode is clicked
 	$wpiaToggleButton.on('click',function(e){
@@ -91,42 +126,6 @@ $(document).ready(function (){
 
 // on load
 $(window).load(function() {
-
-	// function for expanding targeted widgets in the admin
-	// differs from generic target script to handle
-	function expandTargetedWidget() {
-		target = window.location.hash;
-
-		if( ! target )
-			return;
-
-		//strip "#"
-		target = target.replace(/^.*#wpia-/, '');
-
-		$targetedWidget = $('.widget[id$=' + target + ']');
-
-		// Stop if nothing's targeted
-		if( !$targetedWidget.exists() )
-			return;
-
-		$targetedSidebar = $targetedWidget.parents('.widgets-holder-wrap');
-
-		// Expand sidebar of targeted widget if closed
-		if( $targetedSidebar.hasClass('closed') ) {
-			// expand sidebar
-			$('.sidebar-name-arrow', $targetedSidebar).trigger('click');
-		}
-
-		// scroll to the widget
-	    $('html, body').animate({
-	        scrollTop: $targetedWidget.offset().top - 50 // -50 accounts for admin bar
-	    }, 750);
-
-	    // expand widget
-		$('.widget-action', $targetedWidget).trigger('click');
-		// add class to mark as targeted
-		$targetedWidget.addClass('wpia-active-widget');
-	}
 
 	if( $('body').hasClass('widgets-php') ) {
 		expandTargetedWidget();
